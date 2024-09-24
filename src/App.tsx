51f7a2d9071badbe029cb71bcc074f1986c1178c
jsalas39 <jsalas39@cuc.edu.co>
2024-09-24 12:41:43 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import miLogo from "../src/assets/img/messi.png"
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <img src={miLogo} alt="Logo" />
+        <h1>¡¡El mejor del mundo!!</h1>
+        <p>El goat</p>
+      </div>
+    </>
+  )
+}
+
+export default App
